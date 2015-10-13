fc83cfffed5e25f255e2a1f809bdad64355f9338
Dan W <dantech88@yahoo.com>
2015-10-13 20:00:57 +0000

    init commit

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,38 @@
+class UserController < ApplicationController
+  
+  #create and login
+  def login_page
+    
+  end
+  
+  def login
+    
+  end
+  
+  def signup_page
+    
+  end
+  
+  def signup
+  
+  end
+  
+  #view
+  def view
+    
+  end
+  
+  #edit
+  def edit_page
+    
+  end
+  
+  def update
+    
+  end
+  
+  #destroy
+  def destroy
+    
+  end
+end
